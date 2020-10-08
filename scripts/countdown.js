26d5bb5d8404779ad816c1cdd22e26e7dc53df98
Gonnachon Guillaume <ggonnachon@gmail.com>
2020-10-08 18:25:52 +0200

    Remove yaml

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -23,24 +23,14 @@ class Countdown {
                     icon: "fas fa-clock",
                     name: "createCd",
                     title: "Create Countdown Timer as Chat message",
-                    onClick: cdTool.cdButtonHandler
+                    onClick: this.cdButtonHandler
                 }
             ]
         }
     }
 
     _addCountdownButton(controls) {
-        controls.push(cdTool.cdButton);
-    }
-
-    __formatTime(time) {
-        const minutes = Math.floor(time / 60);
-        let seconds = time % 60;
-        if(seconds < 10) {
-            seconds = `0${seconds}`;
-        }
-
-        return `${minutes}:${seconds}`
+        controls.push(this.cdButton);
     }
 
     async cdButtonHandler() {
@@ -53,36 +43,47 @@ class Countdown {
                 create: {
                     icon: '<i class="fas fa-check"></i>',
                     label: "Create Countdown",
-                    callback: cb => {
+                    callback: function(cb) {
                         const form = cb.find('#compendium-create'),
                         data = validateForm(form[0]);
                         data.cdId = Date.now();
-                        data.timeLeft = cdTool.__formatTime(data.cdLength);
+                        data.timeLeft = __formatTime(data.cdLength);
                         data.elapsed = 0;
-                        cdTool.createCountdownChat(data);
+                        createCountdownChat(data);
                     }
                 }
             }
         }).render(true);
     }
 
-    async createCountdownChat(data) {
-        const chatTemplatePath = 'modules/countdown/templates/countdown-chat.html';
-        const html = await renderTemplate(chatTemplatePath, data);
-        let message = await ChatMessage.create({content: html})
-        const timerInterval = new CountdownInterval(this.createTimerInterval, 1000, data, message.data._id);
-        //setTimeout(timerInterval.clear.bind(timerInterval), data.cdLength * 1000);
+    
+}
+
+function __formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    if(seconds < 10) {
+        seconds = `0${seconds}`;
     }
 
-    async createTimerInterval(interval, data, messageId) {
-        const chatTemplatePath = 'modules/countdown/templates/countdown-chat.html';
-        data.elapsed += 1;
-        data.timeLeft = cdTool.__formatTime(data.cdLength - data.elapsed);
-        const html = await renderTemplate(chatTemplatePath, data);
-        ChatMessage.update({_id: messageId, content: html})
-        if ((data.cdLength - data.elapsed) <= 0) {
-            interval.clear();
-        }
+    return `${minutes}:${seconds}`
+}
+
+async function createCountdownChat(data) {
+    const chatTemplatePath = 'modules/countdown/templates/countdown-chat.html';
+    const html = await renderTemplate(chatTemplatePath, data);
+    let message = await ChatMessage.create({content: html})
+    const timerInterval = new CountdownInterval(createTimerInterval, 1000, data, message.data._id);
+}
+
+async function createTimerInterval(interval, data, messageId) {
+    const chatTemplatePath = 'modules/countdown/templates/countdown-chat.html';
+    data.elapsed += 1;
+    data.timeLeft = __formatTime(data.cdLength - data.elapsed);
+    const html = await renderTemplate(chatTemplatePath, data);
+    ChatMessage.update({_id: messageId, content: html})
+    if ((data.cdLength - data.elapsed) <= 0) {
+        interval.clear();
     }
 }
 
@@ -93,3 +94,4 @@ cdTool.createButton();
 Hooks.on("getSceneControlButtons", controls => {
     cdTool._addCountdownButton(controls)
 });
+
